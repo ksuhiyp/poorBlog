5c3a7105c45074857261c378c84552eae8fb0716
suhayb <ksuhiyp@gmail.com>
2020-05-14 21:05:02 +0300

    Transform UserParam into UserEntity

--- src/common/decorators/user.decorator.ts
+++ src/common/decorators/user.decorator.ts
@@ -1,8 +1,7 @@
 import { createParamDecorator } from '@nestjs/common';
+import { UserEntity } from 'src/entities/user.entity';
+import { plainToClass } from 'class-transformer';
 
-export const UserParam = createParamDecorator(
-  (data: unknown, request: any) => {
-
-    return request.user;
-  },
-);
+export const UserParam = createParamDecorator((data: unknown, request: any) => {
+  return plainToClass(UserEntity, request.user);
+});
