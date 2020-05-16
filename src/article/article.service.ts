5818a7d09b013bbdff3cab7153107537a223581d
suhayb <ksuhiyp@gmail.com>
2020-05-16 21:42:27 +0300

    Change draftArticle param type to Partial<Entitiy>

--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -12,110 +12,110 @@ import { AwsService } from 'src/common/services/aws.service';
 import { ImageEntity } from 'src/entities/image.entity';
 @Injectable()
 export class ArticleService {
-    constructor(
-        @InjectRepository(ArticleEntity)
-        private repo: Repository<ArticleEntity>,
-        @InjectRepository(TagEntity) private tagRepo: Repository<TagEntity>,
-        @InjectRepository(PosterEntity)
-        private posterRepo: Repository<PosterEntity>,
-        @InjectRepository(ImageEntity)
-        private imageRepo: Repository<ImageEntity>,
-        private aws: AwsService,
-    ) {}
-    getArticle(query?: GetArticleByIdOrSlugQuery): Promise<ArticleEntity> {
-        return this.repo.findOneOrFail({
-            where: {
-                ...query,
-            },
-            relations: ['author'],
+  constructor(
+    @InjectRepository(ArticleEntity)
+    private repo: Repository<ArticleEntity>,
+    @InjectRepository(TagEntity) private tagRepo: Repository<TagEntity>,
+    @InjectRepository(PosterEntity)
+    private posterRepo: Repository<PosterEntity>,
+    @InjectRepository(ImageEntity)
+    private imageRepo: Repository<ImageEntity>,
+    private aws: AwsService,
+  ) {}
+  getArticle(query?: GetArticleByIdOrSlugQuery): Promise<ArticleEntity> {
+    return this.repo.findOneOrFail({
+      where: {
+        ...query,
+      },
+      relations: ['author'],
+    });
+  }
+  getArticles(query?: GetArticlesQuery): Promise<ArticleEntity[]> {
+    return this.repo.find({ ...query, relations: ['author'] });
+  }
+  async createArticle(drafteArticle: Partial<ArticleEntity>, author: UserEntity): Promise<ArticleEntity> {
+    const articleDraftEntity = this.repo.create(drafteArticle);
+    articleDraftEntity.author = author;
+    return this.repo.save(articleDraftEntity);
+  }
+  async updateArticle(id: number, newArticle: Partial<ArticleEntity>, user: Partial<UserEntity>): Promise<ArticleEntity> {
+    const article = await this.repo.findOneOrFail(id, {
+      relations: ['author'],
+    });
+    if (!article.isArticleAuthor(user)) {
+      throw new ForbiddenException("You don't own the article");
+    }
+    const TagEntities = await this.initTaglistEntities(newArticle.tags);
+    newArticle.tags = TagEntities;
+    newArticle = this.repo.merge(article, newArticle);
+    return this.repo.save(newArticle);
+  }
+
+  async patchArticlePoster(articleId: number, poster: MulterS3File) {
+    const article = await this.repo.findOneOrFail(articleId, {
+      relations: ['poster'],
+    });
+
+    const posterEntity = this.posterRepo.create(plainToClass(PosterEntity, poster));
+
+    if (article.poster) {
+      await this.aws.deleteObject(article.poster.bucket, article.poster.key);
+    }
+
+    await this.posterRepo.delete(article.poster.id);
+
+    article.poster = posterEntity;
+    return this.repo.save(article);
+  }
+
+  async patchArticleImages(articleId: number, image: MulterS3File): Promise<ArticleEntity> {
+    const article = await this.repo.findOneOrFail(articleId);
+    const imageEntity = this.imageRepo.create(image);
+    article.images.push(imageEntity);
+    return this.repo.save(article);
+  }
+
+  async deleteArticleImage(articleId: number, body: DeleteArticleImageDTO) {
+    const article = await this.repo.findOneOrFail(articleId, {
+      relations: ['images'],
+    });
+    const imageToDelete = article.images.find(image => image.location === body.location);
+
+    if (!imageToDelete) {
+      return HttpStatus.NO_CONTENT;
+    }
+
+    await this.aws.deleteObject(imageToDelete.bucket, imageToDelete.key);
+
+    await this.imageRepo.delete({
+      id: imageToDelete.id,
+    });
+
+    return;
+  }
+
+  async deleteArticle(id: number, user: UserRequestDTO): Promise<DeleteResult> {
+    const article = await this.repo.findOneOrFail(id, {
+      relations: ['author'],
+    });
+    if (!article.isArticleAuthor(user)) {
+      throw new ForbiddenException(`User ${user.username} doesn\'t own article ${article.slug}`);
+    }
+    return this.repo.delete(id);
+  }
+
+  private initTaglistEntities(tags: TagEntity[]) {
+    return Promise.all(
+      tags.map(async tag => {
+        const existedTag = await this.tagRepo.findOne({
+          where: { title: tag.title },
         });
-    }
-    getArticles(query?: GetArticlesQuery): Promise<ArticleEntity[]> {
-        return this.repo.find({ ...query, relations: ['author'] });
-    }
-    async createArticle(drafteArticle: ArticleEntity, author: UserEntity): Promise<ArticleEntity> {
-        const articleDraftEntity = this.repo.create(drafteArticle);
-        articleDraftEntity.author = author;
-        return this.repo.save(articleDraftEntity);
-    }
-    async updateArticle(id: number, newArticle: Partial<ArticleEntity>, user: Partial<UserEntity>): Promise<ArticleEntity> {
-        const article = await this.repo.findOneOrFail(id, {
-            relations: ['author'],
-        });
-        if (!article.isArticleAuthor(user)) {
-            throw new ForbiddenException("You don't own the article");
+        if (existedTag) {
+          return existedTag;
+        } else {
+          return this.tagRepo.create(tag);
         }
-        const TagEntities = await this.initTaglistEntities(newArticle.tags);
-        newArticle.tags = TagEntities;
-        newArticle = this.repo.merge(article, newArticle);
-        return this.repo.save(newArticle);
-    }
-
-    async patchArticlePoster(articleId: number, poster: MulterS3File) {
-        const article = await this.repo.findOneOrFail(articleId, {
-            relations: ['poster'],
-        });
-
-        const posterEntity = this.posterRepo.create(plainToClass(PosterEntity, poster));
-
-        if (article.poster) {
-            try {
-                await this.aws.deleteObject(article.poster.bucket, article.poster.key);
-            } catch (error) {
-                Logger.warn(error);
-            }
-        }
-        article.poster = posterEntity;
-
-        return this.repo.save(article);
-    }
-
-    async patchArticleImages(articleId: number, image: MulterS3File): Promise<ArticleEntity> {
-        const article = await this.repo.findOneOrFail(articleId);
-        const imageEntity = this.imageRepo.create(image);
-        article.images.push(imageEntity);
-        return this.repo.save(article);
-    }
-    async deleteArticleImage(articleId: number, body: DeleteArticleImageDTO) {
-        const article = await this.repo.findOneOrFail(articleId, {
-            relations: ['images'],
-        });
-        const imageToDelete = article.images.find(image => image.location === body.location);
-
-        if (!imageToDelete) {
-            return HttpStatus.NO_CONTENT;
-        }
-
-        await this.aws.deleteObject(imageToDelete.bucket, imageToDelete.key);
-
-        await this.imageRepo.delete({
-            id: imageToDelete.id,
-        });
-
-        return;
-    }
-    async deleteArticle(id: number, user: UserRequestDTO): Promise<DeleteResult> {
-        const article = await this.repo.findOneOrFail(id, {
-            relations: ['author'],
-        });
-        if (!article.isArticleAuthor(user)) {
-            throw new ForbiddenException(`User ${user.username} doesn\'t own article ${article.slug}`);
-        }
-        return this.repo.delete(id);
-    }
-
-    private initTaglistEntities(tags: TagEntity[]) {
-        return Promise.all(
-            tags.map(async tag => {
-                const existedTag = await this.tagRepo.findOne({
-                    where: { title: tag.title },
-                });
-                if (existedTag) {
-                    return existedTag;
-                } else {
-                    return this.tagRepo.create(tag);
-                }
-            }),
-        );
-    }
+      }),
+    );
+  }
 }
